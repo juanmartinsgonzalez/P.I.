e0fa99d2ee7213c92ae15ba1c33ef1bdd82200b1
juanmartinsgonzalez <83383144+juanmartinsgonzalez@users.noreply.github.com>
2021-06-22 23:18:23 -0300

    A8-Ex3

---
+++ a8_ex3.js
@@ -0,0 +1,12 @@
+// Crie uma função maiorNumero() que receba N parâmetros, e retorne o menor número entre eles, 
+// utilizando a função Math.min(). Uma observação importante é que a função Math.min() NÃO deve receber um array como parâmetro, 
+// sendo portanto, necessário expandir os valores.
+
+x = 20
+y = 30
+function maiorNumero() {
+    z = Math.min(x ,y)
+    return
+}
+maiorNumero()
+console.log(z)
